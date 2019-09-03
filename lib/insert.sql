f0f76ed72d1c16682b1e467d9a952a663bd96531
Angus Morrison <contact@angus-morrison.com>
2019-09-03 13:04:22 +0100

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Mr. Chocolate", 13, "m", "brown", "gentle", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Rowdy", 8, "m", "black", "rowdy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Tabitha", 22, "f", "brown", "lazy", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Sergeant Brown", 12, "m", "white", "aggressive", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Melissa", 23, "f", "white", "gentle", 0);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Grinch", 16, "m", "black", "mean", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES ("Wendy", 4, "f", "black", "timid", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+VALUES (NULL, 8, "f", "black", "aggressive", 0);
